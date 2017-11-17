45841770da7dff5080038862cef2987c365745c3
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-17 19:39:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,9 +3,9 @@ def turn(board)
   user_input = gets.strip
   display_board(board)
   input_to_index(user_input)
-  valid_move?(board, input,value="X")
+  valid_move?(board, index,value="X")
 
-  move(board, input, value)
+  move(board, index, value)
 end
 
 
