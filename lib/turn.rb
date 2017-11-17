89b4e552627ffd0a65e5957672bdc8743892f262
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-17 19:11:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,28 @@ def display_board(board)
   end
   puts rows.join("\n" + '-'*11 + "\n")
 end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  if (user_input === "invalide")
+    return -1
+  else
+  input = user_input.to_i
+  input = input - 1
+ end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+  puts board
+end
 
 def valid_move?(board, index)
   if ((index.between?(0, 8)  && board[index] == " ") ||
